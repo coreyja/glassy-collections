70dfd0c1edb041fc7896bbbaee0b743c819e03b4
Corey Alexander <corey@coreyja.com>
2016-01-17 23:44:21 -0500

    Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  root 'home#index'
 end
