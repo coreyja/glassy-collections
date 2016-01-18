805800fd8004f2f8a5ffa7c933c6d311f9d755f1
Corey Alexander <corey@coreyja.com>
2016-01-18 02:51:37 -0500

    Fix for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'timer.glassyauctions.com' }
+
+  Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  Rails.application.config.secret_token = ENV["SECRET_TOKEN"]
 end
