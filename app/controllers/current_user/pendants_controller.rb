30879625f825ba64b4c3568a4cce2e6e42adaae3
Corey Alexander <corey@coreyja.com>
2016-01-23 18:11:26 -0500

    Rubocop fixes

--- app/controllers/current_user/pendants_controller.rb
+++ app/controllers/current_user/pendants_controller.rb
@@ -1,26 +1,28 @@
-class CurrentUser::PendantsController < ApplicationController
-  before_action :require_login
+module CurrentUser
+  class PendantsController < ApplicationController
+    before_action :require_login
 
-  def index
-    @pendants = current_user.pendants
-  end
-
-  def new
-    @pendant = current_user.pendants.new
-  end
-
-  def create
-    @pendant = current_user.pendants.new create_params
-    if @pendant.save
-      redirect_to action: :index
-    else
-      render :new
+    def index
+      @pendants = current_user.pendants
     end
-  end
 
-  private
+    def new
+      @pendant = current_user.pendants.new
+    end
 
-  def create_params
-    params.require(:pendant).permit(:name, :user_id, :artist_id)
+    def create
+      @pendant = current_user.pendants.new create_params
+      if @pendant.save
+        redirect_to action: :index
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def create_params
+      params.require(:pendant).permit(:name, :user_id, :artist_id)
+    end
   end
 end
--- app/helpers/current_user/pendant_records_helper.rb
+++
@@ -1,2 +0,0 @@
-module CurrentUser::PendantRecordsHelper
-end
