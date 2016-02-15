2259f9f9d1ddb819f084bf90023352e666042784
Corey Alexander <corey@coreyja.com>
2016-02-15 18:30:40 -0500

    Rubocop fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
