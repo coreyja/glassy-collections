dab394c57868a2fb4f220d262c9cbce117d78cf1
Corey Alexander <corey@coreyja.com>
2016-02-21 18:37:37 -0500

    So not using AssetPipeline anymore. I manually copied the output directly into the temple so I can deploy for now

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,7 +7,11 @@ class Photo < ActiveRecord::Base
   validates_attachment_content_type :photo, content_type: %r{\Aimage/.*\Z}
 
   def data_uri(style=nil)
-    base64 = Base64.encode64(open(photo.url(style)).read.to_s).gsub(/\s+/, "")
+    if Rails.env.development?
+      base64 = Base64.encode64(open(photo.path(style)).read.to_s).gsub(/\s+/, "")
+    else
+      base64 = Base64.encode64(open(photo.url(style)).read.to_s).gsub(/\s+/, "")
+    end
     "data:#{photo.content_type};base64,#{Rack::Utils.escape(base64)}"
   end
 end
