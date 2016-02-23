8aef2b4612e9e2707657280e472535d8d207f5d8
Corey Alexander <corey@coreyja.com>
2016-02-23 00:26:44 -0500

    Set the Timezone to EST so it\'s a least right for me :-p

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module GlassyCollections
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.compile = true
+
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
