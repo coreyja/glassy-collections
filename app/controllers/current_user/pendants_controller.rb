2e7301d75734856dbe83be14cb02b16a14da1605
Corey Alexander <coreyja@gmail.com>
2016-09-25 18:13:23 -0400

    Add edit show and update pendant routes

--- app/controllers/current_user/pendants_controller.rb
+++ app/controllers/current_user/pendants_controller.rb
@@ -7,12 +7,30 @@ module CurrentUser
       @chart_data = current_user.pendant_records.group(:pendant).count.sort.map { |k, v| [k.name, v] }
     end
 
+    def show
+      @pendant = current_user.pendants.find params[:id]
+    end
+
     def new
       @pendant = current_user.pendants.new
     end
 
+    def edit
+      @pendant = current_user.pendants.find params[:id]
+      render :new
+    end
+
     def create
       @pendant = current_user.pendants.new create_params
+      if @pendant.save
+        redirect_to action: :index
+      else
+        render :new
+      end
+    end
+
+    def update
+      @pendant = current_user.pendants.find params[:id]
       if @pendant.save
         redirect_to action: :index
       else
