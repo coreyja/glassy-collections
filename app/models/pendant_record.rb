5b93e5bda7f357ad7efaccb38cb5d96eb0d55ab1
Corey Alexander <coreyja@gmail.com>
2016-10-09 20:55:00 -0400

    Remove scopes that weren't used

--- app/models/pendant_record.rb
+++ app/models/pendant_record.rb
@@ -4,9 +4,6 @@ class PendantRecord < ActiveRecord::Base
   belongs_to :pendant
   belongs_to :photo
 
-  scope :from_date, ->(date) { where('worn_on >= ?', date) }
-  scope :till_date, ->(date) { where('worn_on < ?', date) }
-
   scope :worn_on, ->(day) { where(worn_on: day) }
 
   before_create :assign_worn_on, if: -> { worn_on.nil? }
