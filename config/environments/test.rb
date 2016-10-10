b4983350c3a26e02051a3bf68a52c30fc3f570b8
Corey Alexander <coreyja@gmail.com>
2016-10-09 23:17:20 -0400

    Add config to use ENV vars I just set for Test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+  Rails.application.config.secret_token = ENV['SECRET_TOKEN']
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.middleware.use Clearance::BackDoor
