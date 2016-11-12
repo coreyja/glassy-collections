165b997e3446ecf8a763ee5ff5c55dd70067be82
Corey Alexander <coreyja@gmail.com>
2016-11-12 16:47:43 -0500

    Add Artist.artist_group method to get the artist group for the current artists or init one

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,4 +5,8 @@ class Artist < ApplicationRecord
   has_many :artist_groups, through: :artist_artist_groups
 
   validates :name, presence: true
+
+  def self.artist_group
+    ArtistGroup.joins(:artists).merge(all).first_or_initialize(artists: all)
+  end
 end
