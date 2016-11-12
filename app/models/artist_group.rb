57b6ebb1c206c666db236f9b0f5782b9a061a611
Corey Alexander <coreyja@gmail.com>
2016-11-12 15:49:04 -0500

    Add ArtistGroup#collab?

--- app/models/artist_group.rb
+++ app/models/artist_group.rb
@@ -2,4 +2,8 @@
 class ArtistGroup < ApplicationRecord
   has_many :artist_artist_groups
   has_many :artists, through: :artist_artist_groups
+
+  def collab?
+    artists.count > 1
+  end
 end
--- spec/models/artist_group_spec.rb
+++ spec/models/artist_group_spec.rb
@@ -2,5 +2,32 @@
 require 'rails_helper'
 
 RSpec.describe ArtistGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#collab?' do
+    let(:artists) { FactoryGirl.create_list(:artist, artist_count) }
+    subject { FactoryGirl.create(:artist_group, artists: artists) }
+
+    context 'when there is only 1 artist' do
+      let(:artist_count) { 1 }
+
+      it 'is false' do
+        expect(subject.collab?).to eq false
+      end
+    end
+
+    context 'when there are 2 artists' do
+      let(:artist_count) { 2 }
+
+      it 'is true' do
+        expect(subject.collab?).to eq true
+      end
+    end
+
+    context 'when there are 3 artists' do
+      let(:artist_count) { 3 }
+
+      it 'is true' do
+        expect(subject.collab?).to eq true
+      end
+    end
+  end
 end
