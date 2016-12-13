164228ba453b1cfa5931072ba2461edfb4b9240a
Corey Alexander <coreyja@gmail.com>
2016-12-12 21:43:53 -0500

    Add link to u2f login on sign in page. Add factory

--- spec/factories/u2f_registrations.rb
+++ spec/factories/u2f_registrations.rb
@@ -2,5 +2,9 @@
 FactoryGirl.define do
   factory :u2f_registration do
     user
+    key_handle 'FAKE_KEY_HANDLE'
+    public_key 'FAKE_PUBLIC_KEY'
+    certificate 'FAKE_CERTIFICATE'
+    counter 0
   end
 end
