cea357bb88396a6b6837a78ac5ace5b16b8b5256
Corey Alexander <coreyja@gmail.com>
2016-12-27 00:32:17 -0500

    Add open-app action

--- app/assets/javascripts/sw.js
+++ app/assets/javascripts/sw.js
@@ -1,37 +1,44 @@
 console.log('Started', self);
 self.addEventListener('install', function(event) {
-    self.skipWaiting();
-    console.log('Installed', event);
+  self.skipWaiting();
+  console.log('Installed', event);
 });
 self.addEventListener('activate', function(event) {
-    console.log('Activated', event);
+  console.log('Activated', event);
 });
 self.addEventListener('push', function(event) {
-    if (event.data) {
-        var json = event.data.json();
-        self.registration.showNotification(json.title, json);
-    }
+  if (event.data) {
+    var json = event.data.json();
+    self.registration.showNotification(json.title, json);
+  }
 });
 self.addEventListener('notificationclick', function(event) {
-    console.log('Notification click: tag ', event.notification.tag);
-    event.notification.close();
+  console.log('Notification click: tag ', event.notification.tag);
+  event.notification.close();
 
-    var action = event.action || 'record-pendant';
+  var action = event.action || 'open-app';
 
-    if (action === 'record-pendant') {
-        var url = 'https://glassycollections.com/';
-        event.waitUntil(clients.matchAll({
-                includeUncontrolled: true,
-                type: 'window'
-            }).then(
-            function(activeClients) {
-                if (activeClients.length > 0) {
-                    activeClients[0].navigate(url);
-                    activeClients[0].focus();
-                } else {
-                    clients.openWindow(url);
-                }
-            })
-        );
-    }
+  var url = false;
+
+  if (action === 'open-app') {
+    url = 'https://glassycollections.com/';
+  } else if (action === 'record-pendant') {
+    url = 'https://glassycollections.com/my/pendant_records/new';
+  }
+
+  if (url) {
+    event.waitUntil(clients.matchAll({
+          includeUncontrolled: true,
+          type: 'window'
+        }).then(
+        function(activeClients) {
+          if (activeClients.length > 0) {
+            activeClients[0].navigate(url);
+            activeClients[0].focus();
+          } else {
+            clients.openWindow(url);
+          }
+        })
+    );
+  }
 });
