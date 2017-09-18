ee28f15b59cec9e673490088dbed52e8cdf96d38
Corey Alexander <coreyja@gmail.com>
2017-09-17 20:59:22 -0400

    Add spec of adding a pendant with existing colors

--- spec/system/user_creates_pendant_spec.rb
+++ spec/system/user_creates_pendant_spec.rb
@@ -17,6 +17,28 @@ RSpec.describe 'User creates Pendant', js: true do
     expect(page).to have_link 'Cool Pendy'
   end
 
+  context 'when colors exist' do
+    let!(:color) { FactoryGirl.create :color, name: 'Fun Color' }
+
+    scenario 'with the color availible' do
+      visit root_path(as: user)
+      click_link 'Pendants'
+      click_link 'New Pendant'
+
+      fill_select_and_take_first_result 'Who made this pendant?', 'Artist Name'
+      fill_select_and_take_first_result 'Select Collection...', 'Personal Collection'
+      fill_select_and_take_first_result 'What colors is this piece made of?', ''
+      fill_in 'glass_art_piece_name', with: 'Cool Pendy'
+      expect { click_button 'Create Glass art piece' }.to change { GlassArtPiece.count }.by(1)
+
+      expect(page).to have_link 'Cool Pendy'
+      click_link 'Cool Pendy'
+
+      expect(page).to have_content 'Artist Name'
+      expect(page).to have_content 'Fun Color'
+    end
+  end
+
   private
 
   def fill_select_and_take_first_result(placeholder, query)
